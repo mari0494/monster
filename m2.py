8b6005cdb664df503ed8d89b92d9b8cbf136af6a
mari0494 <62379568+mari0494@users.noreply.github.com>
2021-10-31 21:47:26 +0300

    Add files via upload

---
+++ m2.py
@@ -0,0 +1,8 @@
+from main import *
+# Основная программа
+Frank = Monster("Фрэнки", "необычный")
+Frank.show()
+Albert = GMonster("Альберт", "задумчивый")
+Albert.show()
+Sigmund = SMonster("Зигмунд", "веселый")
+Sigmund.show()
